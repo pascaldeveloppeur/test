7c723b71147db9802e0bf12dfc00f122a5d69fc3
pascalDev <pascalyakouyami@gmail.com>
2024-06-22 11:07:01 +0100

    u

---
+++ Ecommerce-product-service/src/main/java/com/pascaldev/Ecommerce_product_service/EcommerceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pascaldev.Ecommerce_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProductServiceApplication.class, args);
+	}
+
+}
