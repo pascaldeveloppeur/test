cf47bddd7f2799c8cf13bc8895fb2c9cef222740
pascalDev <pascalyakouyami@gmail.com>
2024-06-22 11:06:36 +0100

    u

---
+++ Ecommerce-payment-service/src/main/java/com/pascaldev/Ecommerce_payment_service/EcommercePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pascaldev.Ecommerce_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePaymentServiceApplication.class, args);
+	}
+
+}
---
+++ Ecommerce-payment-service/src/test/java/com/pascaldev/Ecommerce_payment_service/EcommercePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascaldev.Ecommerce_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
