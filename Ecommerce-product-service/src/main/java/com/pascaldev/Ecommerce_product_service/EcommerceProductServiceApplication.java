8ebe67d02d56cd35fab3b0d54a3d4eebb22a0a14
pascalDev <pascalyakouyami@gmail.com>
2024-07-03 13:38:24 +0100

    u

--- Ecommerce-product-service/src/main/java/com/pascaldev/Ecommerce_product_service/EcommerceProductServiceApplication.java
+++ Ecommerce-product-service/src/main/java/com/pascaldev/Ecommerce_product_service/EcommerceProductServiceApplication.java
@@ -2,12 +2,22 @@ package com.pascaldev.Ecommerce_product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition
+@Slf4j
 public class EcommerceProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceProductServiceApplication.class, args);
+		log.info("E-CommerceProduct service started!");
 	}
 
 }
