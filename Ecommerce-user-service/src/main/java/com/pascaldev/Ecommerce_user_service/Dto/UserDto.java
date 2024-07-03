81a078ad4d3ad51b7b67c4a534bd43fa0ce47563
pascalDev <pascalyakouyami@gmail.com>
2024-07-03 13:01:08 +0100

    u

--- Ecommerce-user-service/src/main/java/com/pascaldev/Ecommerce_user_service/Dto/UserDto.java
+++ Ecommerce-user-service/src/main/java/com/pascaldev/Ecommerce_user_service/Dto/UserDto.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @Data
 public class UserDto {
 	
+	private Long id;
 	private String username;
 	private String password;
 	private String email;
--- Ecommerce-user-service/src/main/java/com/pascaldev/Ecommerce_user_service/EcommerceUserServiceApplication.java
+++ Ecommerce-user-service/src/main/java/com/pascaldev/Ecommerce_user_service/EcommerceUserServiceApplication.java
@@ -2,12 +2,21 @@ package com.pascaldev.Ecommerce_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition
+@Slf4j
 public class EcommerceUserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceUserServiceApplication.class, args);
+		log.info("E-CommerceUser service started!");
 	}
 
 }
