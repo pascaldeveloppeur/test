d1f851bf7eeea625d213c058c6feb21e07cb2430
pascalDev <pascalyakouyami@gmail.com>
2024-09-20 15:36:15 +0200

    u

--- Ecommerce-multitenant-jpa/src/main/resources/db/migration/V1_0_20240100__init_user.sql
+++ Ecommerce-multitenant-jpa/src/main/resources/db/migration/V1_0_20240100__init_user.sql
@@ -1,10 +1,15 @@
 create table "ecommerce-user" (
+        "enabled" boolean not null,
+        "created_at" timestamp(6),
         "id" bigint not null,
-        "name" varchar(20),
+        "updated_at" timestamp(6),
+        "username" varchar(20),
         "email" varchar(255) not null unique,
+        "first_name" varchar(255),
+        "last_name" varchar(255),
         "password" varchar(255) unique,
+        "role" varchar(255) check ("role" in ('ADMIN','CLIENT')),
         primary key ("id")
     );
 create sequence "ecommerce-user_seq" start with 1 increment by 1
 
- 
