f57837c64bc6d5b5b35f3ea57c082af45a262dcc
pascalDev <pascalyakouyami@gmail.com>
2024-10-02 19:49:31 +0200

    u

--- Ecommerce-multitenant-jpa/src/main/resources/db/migration/V1_0_20240101__init_product.sql
+++ Ecommerce-multitenant-jpa/src/main/resources/db/migration/V1_0_20240101__init_product.sql
@@ -1,9 +1,13 @@
-create table "product" (
-        "price" float(53),
-        "quantity" integer,
+   create table "product" (
+        "category_id" bigint,
+        "created_at" timestamp(6),
         "id" bigint not null,
+        "price_id" bigint unique,
+        "stock_id" bigint unique,
+        "updated_at" timestamp(6),
         "description" varchar(100),
         "name" varchar(255),
+        "sku" varchar(255),
         primary key ("id")
     );
 create sequence "product_seq" start with 1 increment by 1
